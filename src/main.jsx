de21281fd2626ddff4d937af6fa9e5819a55b92c
Vinicius Farias <fsvinicius9@gmail.com>
2024-04-22 16:57:52 -0300

    Create todo exercise

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card from './components/Card.jsx';
+import CreateTodo from './components/CreateTodo.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CreateTodo></CreateTodo>
   </React.StrictMode>,
 )
